3623140192849b9efc0bd17fb22c16647d48714f
Errorline10 <104586300+Errorline10@users.noreply.github.com>
2026-06-17 15:22:37 -0400

    removes the image white space - athletics

--- script/liveUpdateTemplateAthletics.js
+++ script/liveUpdateTemplateAthletics.js
@@ -11,7 +11,7 @@ document.getElementById(id+'-email-signature-code').innerHTML = `
     <a href="https://www.fgcu.edu" style="color:#0072ce; text-decoration:none;">${formData["department-url"].split('//www.')[1]}</a>
   </td></tr>
   <tr><td style="padding:10px 0;">
-    <img src="https://fgcucdn.fgcu.edu/_resources/images/email-signature/fgcu-athletics-logo.png" alt="FGCU Logo" width="121" height="86px" style="display:block; border:0;">
+    <img src="https://fgcucdn.fgcu.edu/_resources/images/email-signature/fgcu-athletics-logo-2.png" alt="FGCU Logo" width="121" height="86px" style="display:block; border:0;">
   </td></tr>
   <tr><td style="padding:0;">
     <strong>Florida Gulf Coast University</strong>
